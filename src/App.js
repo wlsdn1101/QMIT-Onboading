7d632fbbbddfa6247b6accb6c358c44c1b2b23ac
이진우 <ljw041101@gmail.com>
2022-09-14 15:41:10 +0900

    feat : calendar publishing 완료

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import * as S from './styles'
 
 function App() {
+  const arr = ['(', ')', '%', 'AC', 7, 8, 9, '%', 4, 5, 6,'X', 1, 2, 3, '-', 0, '.', '=', '+'];
+  
   return (
-    <div className="App">
-      
-    </div>
+    <S.Container>
+      <S.InputWrap></S.InputWrap>
+      <S.BtnContainer>
+        {arr.map((str, i)=>(<S.Btn isNum={typeof(str)==='number'} key={i}>{str}</S.Btn>))}
+      </S.BtnContainer>
+    </S.Container>
   );
 }
 

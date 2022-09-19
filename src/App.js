9bae00d0629ee3e8ce29d25d08088dc274a232df
이진우 <ljw041101@gmail.com>
2022-09-19 16:15:52 +0900

    feat : 계산기 사칙연산 및 삭제 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import * as S from "./styles";
+import { addValueFunc, caculation } from "./functions";
 
 function App() {
+  const [deleteValue, setDeleteValue] = useState("AC");
   const [formula, setFormula] = useState("0");
   const [answer, setAnswer] = useState("");
-  //const [isAllClear, setIsAllClear] = useState("AC");
-  const caculatorArr = [
+
+  const caculatorValue = [
     "(",
     ")",
     "%",
-    "AC",
+    deleteValue,
     7,
     8,
     9,
@@ -28,68 +30,7 @@ function App() {
     "+",
   ];
 
-  const caculation = () => {
-    let formulaArr = formula.split(" ");
-    let charLocation;
-
-    while (formulaArr.length !== 1) {
-      charLocation = 0;
-
-      // 곱하기, 나누기의 계산 우선순위를 높이기 위한 조건문
-      if (formulaArr.indexOf("*") > 0) {
-        charLocation = formulaArr.indexOf("*");
-      } else if (formulaArr.indexOf("/") > 0) {
-        charLocation = formulaArr.indexOf("/");
-      }
-
-      if (charLocation !== 0) {
-        if (formulaArr[charLocation] === "*") {
-          formulaArr[charLocation - 1] =
-            Number(formulaArr[charLocation - 1]) *
-            Number(formulaArr[charLocation + 1]);
-        } else {
-          formulaArr[charLocation - 1] =
-            Number(formulaArr[charLocation - 1]) /
-            Number(formulaArr[charLocation + 1]);
-        }
-      } else if (formulaArr[1] === "-") {
-        formulaArr[0] = Number(formulaArr[0]) - Number(formulaArr[2]);
-        charLocation = 1;
-      } else if (formulaArr[1] === "+") {
-        formulaArr[0] = Number(formulaArr[0]) + Number(formulaArr[2]);
-        charLocation = 1;
-      }
-
-      formulaArr.splice(charLocation, charLocation + 1);
-    }
-    return formulaArr[0];
-  };
-
   // onClick에서 이동 후 문자에 따라 값 처리 및 출력
-  const charAddFunc = (btnValue) => {
-    if (answer[answer.length - 1] === "=") {
-      setAnswer(`Ans = ${formula}`);
-    }
-
-    setFormula(`${formula} ${btnValue} `);
-    //setIsAllClear("CE");
-  };
-
-  const numAddFunc = (btnValue) => {
-    let stringTmp = formula;
-    if (
-      formula[formula.length - 1] === "0" &&
-      !Number(formula[formula.length - 2])
-    ) {
-      stringTmp = stringTmp.slice(0, -1);
-      setFormula(stringTmp + btnValue);
-    } else if (answer[answer.length - 1] === "=") {
-      setAnswer("Ans = " + formula);
-      setFormula(btnValue);
-    } else {
-      setFormula(String(stringTmp) + String(btnValue));
-    }
-  };
 
   const clickEqual = () => {
     if (!Number(formula[formula.length - 1])) {
@@ -99,33 +40,40 @@ function App() {
     }
 
     setAnswer(formula + "=");
-    setFormula(String(caculation()));
-    //setIsAllClear("AC");
+    setFormula(String(caculation(formula)));
+    setDeleteValue("AC");
   };
 
-  const onClickBtn = (btnValue) => {
+  const onClickBtn = (value) => {
     // 정답 칸이 비어있는지 확인
     if (answer === "") {
-      setAnswer("Ans=0");
+      setAnswer("Ans = 0");
     }
     // 문자 확인
-    if (btnValue === "=") {
+
+    if (value === "=") {
       clickEqual();
-    } else if (typeof btnValue === "string") {
-      charAddFunc(btnValue);
     } else {
-      numAddFunc(btnValue);
-      //setIsAllClear("CE");
+      const output = addValueFunc({ formula, answer, value });
+      setFormula(output[0]);
+      setDeleteValue("CE");
+
+      if (output[1]) {
+        setAnswer(output[1]);
+      }
     }
   };
+
   return (
     <S.Container>
       <S.InputWrap>
         <S.AnswerBox>{answer}</S.AnswerBox>
-        <S.FormulaBox>{formula}</S.FormulaBox>
+        <S.FormulaBox>
+          <span>{formula}</span>
+        </S.FormulaBox>
       </S.InputWrap>
       <S.BtnContainer>
-        {caculatorArr.map((caculatorMember, i) => (
+        {caculatorValue.map((caculatorMember, i) => (
           <S.Btn
             key={i}
             onClick={() => onClickBtn(caculatorMember)}
---
+++ src/functions.js
@@ -0,0 +1,150 @@
+const sum = (a, b) => {
+  return a + b;
+};
+
+const minus = (a, b) => {
+  return a - b;
+};
+
+const multiple = (a, b) => {
+  return a * b;
+};
+
+const division = (a, b) => {
+  return a / b;
+};
+
+const percentage = (a) => {
+  return a / 100;
+};
+
+const checkSign = ({ preNum, sign, postNum }) => {
+  const newPreNum = Number(preNum);
+  const newPostNum = Number(postNum);
+
+  if (sign === "-") {
+    return minus(newPreNum, newPostNum);
+  }
+
+  if (sign === "+") {
+    return sum(newPreNum, newPostNum);
+  }
+
+  if (sign === "*") {
+    return multiple(newPreNum, newPostNum);
+  }
+
+  if (sign === "/") {
+    return division(newPreNum, newPostNum);
+  }
+
+  if (sign === "%") {
+    return percentage(newPreNum);
+  }
+};
+
+const changeFormulaValue = (formula, signLocaion = 1) => {
+  const newFormula = [...formula];
+  const [preNum, sign, postNum] = newFormula.slice(signLocaion - 1);
+
+  newFormula[signLocaion - 1] = checkSign({ preNum, sign, postNum });
+  newFormula.splice(signLocaion, 2);
+
+  return newFormula;
+};
+
+const indexOfPrioritySign = (formula) => {
+  if (formula.indexOf("%") > 0) {
+    return formula.indexOf("%");
+  }
+
+  if (formula.indexOf("*") > 0) {
+    return formula.indexOf("*");
+  }
+
+  if (formula.indexOf("/") > 0) {
+    return formula.indexOf("/");
+  }
+
+  return 1;
+};
+
+export const caculation = (formula) => {
+  let newFormula = formula.split(" ");
+
+  while (newFormula.length !== 1) {
+    newFormula = changeFormulaValue(
+      newFormula,
+      indexOfPrioritySign(newFormula),
+    );
+  }
+
+  return newFormula[0];
+};
+
+// 계산식 삭제 함수
+const deleteAll = (formula) => {
+  return ["0", `Ans = ${formula}`];
+};
+
+const deletePreValue = ({ formula, answer }) => {
+  if (formula === 0) {
+    return;
+  }
+
+  if (formula.slice(0, -1) === "") {
+    return ["0", answer];
+  }
+
+  return [formula.slice(0, -1), answer];
+};
+
+// 식에 숫자, 문자 추가하는 함수
+const addChar = ({ formula, answer, value }) => {
+  let newAnswer = answer;
+
+  if (answer[answer.length - 1] === "=") {
+    newAnswer = `Ans = ${formula}`;
+  }
+
+  return [`${formula} ${value} `, newAnswer];
+};
+
+const addNumber = ({ formula, answer, value }) => {
+  let newFormula = formula;
+
+  if (
+    formula[formula.length - 1] === "0" &&
+    !Number(formula[formula.length - 2])
+  ) {
+    newFormula = newFormula.slice(0, -1);
+    return [newFormula + value];
+  } else if (answer[answer.length - 1] === "=") {
+    return [value, `Ans = ${newFormula}`];
+  } else {
+    return [`${newFormula}${value}`];
+  }
+};
+
+const checkValueType = ({ formula, answer, value }) => {
+  if (typeof value === "number" || value === ".") {
+    return addNumber({ formula, answer, value });
+  }
+
+  if (typeof value === "string") {
+    return addChar({ formula, answer, value });
+  }
+};
+
+export const addValueFunc = ({ formula, answer, value }) => {
+  if (value === "AC") {
+    return deleteAll({ formula, answer });
+  }
+
+  if (value === "CE") {
+    return deletePreValue(formula);
+  }
+  let [formulaValue, answerValue] = checkValueType({ formula, answer, value });
+
+  return [formulaValue, answerValue];
+};
